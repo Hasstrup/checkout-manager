5680cc126698192da5943a82b2cea2662988f1a0
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2024-10-17 01:12:06 +0100

    chore: set up gh actions to run specs

--- lib/checkout/core/concerns/cursor_operations.rb
+++ lib/checkout/core/concerns/cursor_operations.rb
@@ -4,11 +4,17 @@ module Checkout
   module Core
     module Concerns
       module CursorOperations
+        # Takes a cart entry and returns a new cursor with sensible defaults.
+        #
+        # @param [Checkout::Cart::StoreEntry] cart_entry
+        # @returns [Checkout::Core::CartSummator::Cursor]
         def build_for(cart_entry)
-          new(entry: cart_entry,
-              remainder: cart_entry.amount,
-              current_cost: 0,
-              applied_discounts: [])
+          new(
+            entry: cart_entry,
+            remainder: cart_entry.amount,
+            current_cost: 0,
+            applied_discounts: []
+          )
         end
       end
     end
